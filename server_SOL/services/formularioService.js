806e0f84f3a003c167958430c694b8dbaa1ccd09
anabea02 <anabeatriz0242@gmail.com>
2023-12-17 01:18:29 +0000

    api formulario

--- server_SOL/services/formularioService.js
+++ server_SOL/services/formularioService.js
@@ -10,6 +10,30 @@ module.exports = {
             res.send(results.rows)
         });
     },
+    ResponderFormulario: (req, res) => {
+        client.query('call responder_formulario($1, $2, $3)', [req.query.id_form, req.query.id_med, req.query.obsv], (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.send(results.rows)
+        });
+    },
+    PrescreverForm: (req, res) => {
+        client.query('call inserir_prescricao_form($1, $2)', [req.query.id_form, req.query.id_med, req.query.obsv], (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.send(results.rows)
+        });
+    },
+    PrescreverExameForm: (req, res) => {
+        client.query('call responder_formulario($1, $2, $3)', [req.query.id_form, req.query.id_med, req.query.obsv], (error, results) => {
+            if (error) {
+                throw error
+            }
+            res.send(results.rows)
+        });
+    },
     GetFormulariosNaoRespondidos: (req, res) => {
         client.query("SELECT * FROM formularios where estado_formulario = 'Submetido'", (error, results) => {
             if (error) {
@@ -19,7 +43,7 @@ module.exports = {
         });
     },
     GetFormulariosRespondidos: (req, res) => {
-        client.query("SELECT * FROM formularios where estado_formulario = 'Respondido'", (error, results) => {
+        client.query("SELECT f.*, fp.* FROM formularios f INNER JOIN prescricoes_formulario pf ON f.id_utente = pf.id_utente AND estado_formulario = 'Respondido' AND pf.id_medico = $1", [req.query.id_med], (error, results) => {
             if (error) {
                 throw error
             }
